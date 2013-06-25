45ae2f225d443442f6ba13a39d4a680a22fb7264
Սարգսյան Հովհաննես <sarhov13@yahoo.com>
2013-06-25 20:50:42 +0400

    update

--- functions.js
+++ functions.js
@@ -1,3 +1,46 @@
 jQuery(document).ready(function($) {
-	
+
+	// the top mini cart open and close
+
+	var minicart = $('.mini-cart-info').hide();
+	var openme = $('.cart');
+   openme.click(function(){
+   minicart.slideDown();
+   openme.mouseleave(function(){
+   			minicart.slideUp();
+   		});
+   });
+
+
+
+
+   // The  sub menu open close
+
+
+
+$('li.item ul').each(function(index) {
+$(this).prev().addClass('idCatSubcat')});
+$('li.item a').after('<span></span>');
+$('li.item ul').css('display','none');
+$('li.item ul.active').css('display','block');
+$('li.item ul').each(function(index) {
+$(this).prev().addClass('close').click(function() {
+if (
+$(this).next().css('display') == 'none') {
+$(this).next().slideDown(400, function () {
+$(this).prev().removeClass('collapsed').addClass('expanded');
+});
+}else {
+$(this).next().slideUp(400, function () {
+$(this).prev().removeClass('expanded').addClass('collapsed');
+$(this).find('ul').each(function() {
+$(this).hide().prev().removeClass('expanded').addClass('collapsed');
+});
+});
+}
+return false;
+});
+});
+
+
 });
