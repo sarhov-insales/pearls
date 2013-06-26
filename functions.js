d01aca04abe2fd18c04f10030cb16e1266083f3d
Սարգսյան Հովհաննես <sarhov13@yahoo.com>
2013-06-27 00:10:34 +0400

    update

--- functions.js
+++ functions.js
@@ -87,7 +87,40 @@ current:'Товар {current} из {total}'
 
 $('.addToCart').click(function () {
 var cart = $('.cart');
-var imgtodrag = $(this).closest('.itembox').find("img").eq(0);
+var imgtodrag = $(this).closest('.itembox').find("img");
+var imgclone = imgtodrag.clone()
+.offset({
+top: imgtodrag.offset().top,
+left: imgtodrag.offset().left
+})
+.css({
+'opacity': '0.5',
+'position': 'absolute',
+'height': '150px',
+'width': '150px',
+'z-index': '100'
+})
+
+.appendTo($('body'))
+.animate({
+'top': cart.offset().top + 10,
+'left': cart.offset().left + 10,
+'width': 75,
+'height': 75
+},1000);
+
+imgclone.animate({
+'width': 0,
+'height': 0
+});
+});
+
+
+// the animation for products page
+
+$('.addToCart').click(function () {
+var cart = $('.cart');
+var imgtodrag = $(this).closest('.editor').find("#zoom1 img");
 var imgclone = imgtodrag.clone()
 .offset({
 top: imgtodrag.offset().top,
