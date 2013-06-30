c87d41917cf800d30e22bdf4807787ad98cff7e1
Սարգսյան Հովհաննես <sarhov13@yahoo.com>
2013-06-30 17:02:20 +0400

    update

--- js/functions.js
+++ js/functions.js
@@ -128,6 +128,14 @@ imgclone.animate({
         // Configuration goes here
     });
 
+    //the slide show in product
+
+$("area[data-rel^='prettyPhoto']").prettyPhoto();
+
+$(".gallery:first a[rel^='prettyPhoto']").prettyPhoto({animation_speed:'normal',slideshow:3000, autoplay_slideshow: false});
+$(".gallery:gt(0) a[rel^='prettyPhoto']").prettyPhoto({animation_speed:'fast',slideshow:10000, hideflash: true});
+
+
 
 
 // the tabs
